cc7e4193e35d74c070eb29cccb4d63ccfe1b3d6d
fan.li2 <1406433326@qq.com>
2024-05-26 10:20:08 +0800

    update: 建造者模式

--- DesignPattern/FactoryWays/BuilderPattern.cpp
+++ DesignPattern/FactoryWays/BuilderPattern.cpp
@@ -4,47 +4,55 @@
 #include <memory>
 using namespace std;
 
-class PersonBuilder {
+class PersonBuilder
+{
 public:
     virtual void buildName() = 0;
     virtual void buildAge() = 0;
     virtual void buildGender() = 0;
 };
 
-class PersonTom: public PersonBuilder {
-    public:
-        void buildName() {
-            std::cout << "Name: Tom" << std::endl;
-        }
-        void buildAge() {
-            std::cout << "Age: 25" << std::endl;
-        }
-        void buildGender() {
-            std::cout << "Gender: Male" << std::endl;
-        }
+class PersonTom : public PersonBuilder
+{
+public:
+    void buildName()
+    {
+        std::cout << "Name: Tom" << std::endl;
+    }
+    void buildAge()
+    {
+        std::cout << "Age: 25" << std::endl;
+    }
+    void buildGender()
+    {
+        std::cout << "Gender: Male" << std::endl;
+    }
 };
 
-
-class PersonJerry: public PersonBuilder {
-    public:
-        void buildName() {
-            std::cout << "Name: Jerry" << std::endl;
-        }
-        void buildAge() {
-            std::cout << "Age: 30" << std::endl;
-        }
-        void buildGender() {
-            std::cout << "Gender: Female" << std::endl;
-        }
+class PersonJerry : public PersonBuilder
+{
+public:
+    void buildName()
+    {
+        std::cout << "Name: Jerry" << std::endl;
+    }
+    void buildAge()
+    {
+        std::cout << "Age: 30" << std::endl;
+    }
+    void buildGender()
+    {
+        std::cout << "Gender: Female" << std::endl;
+    }
 };
 
-
-
-class PersonDirector {
+class PersonDirector
+{
 public:
     PersonDirector(shared_ptr<PersonBuilder> builder) : builder_(builder) {}
 
-    void createPerson() {
+    void createPerson()
+    {
         builder_->buildName();
         builder_->buildAge();
         builder_->buildGender();
@@ -54,10 +62,8 @@ private:
     shared_ptr<PersonBuilder> builder_;
 };
 
-
-
-
-int main() {
+int main()
+{
     shared_ptr<PersonBuilder> builder = make_shared<PersonTom>();
     shared_ptr<PersonDirector> director = make_shared<PersonDirector>(builder);
     director->createPerson();
